1175909cad072154fb720073d724a3db9fd22953
Chris McDonough <chrism@plope.com>
2008-10-06 01:19:42 +0000

    Evolve to package_version at first call.

--- repoze/lemonade/evolve.py
+++ repoze/lemonade/evolve.py
@@ -1,13 +1,20 @@
 from pkg_resources import EntryPoint
 import transaction
 
+KEY = 'repoze.lemonade.evolve'
+
 def evolve(package, ob, transaction=transaction):
     # transaction overrideable for unit tests
     package_name = package.__name__
     package_version = getattr(package, '__version__', 0)
     root = ob._p_jar.root()
-    registry = root.setdefault('repoze.lemonade.evolve', {})
-    db_version = registry.setdefault(package_name, 0)
+    registry = root.setdefault(KEY, {})
+    db_version = registry.get(package_name)
+    if db_version is None:
+        transaction.begin()
+        db_version = registry[package_name] = package_version
+        root[KEY] = registry
+        transaction.commit()
     if not isinstance(package_version, int):
         raise ValueError('package __version__ %s is not an integer' %
                          package_version)
@@ -15,9 +22,10 @@ def evolve(package, ob, transaction=transaction):
         for version in range(db_version+1, package_version+1):
             scriptname = '%s.evolve%s' % (package_name, version)
             evmodule = EntryPoint.parse('x=%s' % scriptname).load(False)
+            transaction.begin()
             evmodule.evolve(ob)
             registry[package_name] = version
-            root['repoze.lemonade.evolve'] = registry
+            root[KEY] = registry
             transaction.commit()
         return version
     return db_version
--- repoze/lemonade/tests/test_evolve.py
+++ repoze/lemonade/tests/test_evolve.py
@@ -13,7 +13,7 @@ class EvolveTests(unittest.TestCase):
         evolve.__version__ = 1
         v = self._callFUT(evolve, ob, t)
         self.assertEqual(v, 1)
-        self.assertEqual(ob.evolved, 1)
+        self.assertEqual(ob.evolved, None)
         self.assertEqual(t.committed, 1)
         reg = root['repoze.lemonade.evolve']
         self.assertEqual(reg['repoze.lemonade.tests.fixtureapp.evolve'], 1)
@@ -33,7 +33,8 @@ class EvolveTests(unittest.TestCase):
         self.assertEqual(reg['repoze.lemonade.tests.fixtureapp.evolve'], 2)
 
     def test_evolve_error(self):
-        root = {}
+        root = {'repoze.lemonade.evolve':
+                {'repoze.lemonade.tests.fixtureapp.evolve':0}}
         ob = DummyPersistent(root)
         t = DummyTransaction()
         from repoze.lemonade.tests.fixtureapp import evolve
@@ -48,7 +49,6 @@ class EvolveTests(unittest.TestCase):
         root = {'repoze.lemonade.evolve':
                 {'repoze.lemonade.tests.fixtureapp.evolve':1}}
         ob = DummyPersistent(root)
-        ob.evolved = None
         t = DummyTransaction()
         from repoze.lemonade.tests.fixtureapp import evolve
         evolve.__version__ = 1
@@ -63,6 +63,7 @@ class Dummy(object):
     pass
 
 class DummyPersistent(object):
+    evolved = None
     def __init__(self, root):
         self._p_jar = Dummy()
         self._p_jar.root = lambda *arg: root
@@ -70,8 +71,12 @@ class DummyPersistent(object):
         
 class DummyTransaction(object):
     committed = 0
+    begun = 0
     def commit(self):
         self.committed = self.committed + 1
+
+    def begin(self):
+        self.begun = self.begun + 1
         
         
         
