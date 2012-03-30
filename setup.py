444852532219205d213c05b6130ebf78e8a54f9d
Tres Seaver <tseaver@palladion.com>
2012-03-30 00:02:20 -0400

    Drop support for Python 2.4 / 2.5.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 
-__version__ = '0.7.6'
+__version__ = '0.8dev'
 
 import os
 import sys
@@ -27,29 +27,16 @@ except:
     README = ''
     CHANGES = ''
 
-if sys.version_info >= (2, 6): #pragma NO COVER Python >= 2.6
-    requires = [
-        'setuptools',
-        'zope.component',
-        'zope.configuration',
-        'zope.interface',
-        ]
+requires = [
+    'setuptools',
+    'zope.component',
+    'zope.configuration',
+    'zope.interface',
+    ]
 
-    tests_require = requires + [
-        'zope.testing',
-        ]
-else: #pragma NO COVER Python < 2.6
-    requires = [
-        'setuptools',
-        'zope.schema<4.0dev',
-        'zope.component<3.11dev',
-        'zope.configuration<3.7dev',
-        'zope.interface<3.8dev',
-        ]
-
-    tests_require = requires + [
-        'zope.testing<4.1dev',
-        ]
+tests_require = requires + [
+    'zope.testing',
+    ]
 
 testing_extras = ['nose', 'coverage']
 
@@ -65,8 +52,6 @@ setup(name='repoze.lemonade',
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.4",
-        "Programming Language :: Python :: 2.5",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: Implementation :: CPython",
